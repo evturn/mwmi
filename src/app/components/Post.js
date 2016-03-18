08bf7f009afbb77784d123c0171c7a0e02b1679e
Evan Turner <evturn@gmail.com>
2016-03-18 12:16:03 -0400

    Move Post html into variables

--- src/app/components/Post.js
+++ src/app/components/Post.js
@@ -8,22 +8,36 @@ export default props => {
     image, author, categories,
     content } = props;
 
+  const date = moment(publishedDate).format('MMM Do YYYY');
+
+  const header = (
+    <div className="entry__header">
+      <div className="entry__header-title">
+        <Link to={{ pathname: `/blog/post/${slug}` }}>{title}</Link>
+      </div>
+      <div className="entry__header-caption">{date}</div>
+    </div>
+  );
+
+  const photo = image !== undefined ? <img className="entry__image" src={image.url} /> : null
+
+  const body = <div className="entry__body" dangerouslySetInnerHTML={{ __html: content.extended }} />;
+
+  const footer = (
+    <div className="entry__footer">
+      <div className="entry__footer-author">By: <Link key={author.id} to={{ pathname: `/blog/authors/${author.name.first}` }}>{author.name.first}</Link></div>
+      <div className="entry__footer-categories">Posted in | {categories.map(item =>
+        <Link key={item.key} to={{ pathname: `/blog/categories/${item.key}` }}>{item.name}</Link>
+      )}</div>
+    </div>
+  );
+
   return (
     <div className="entry" data-ks-editable="if-user-blah-blah-blah">
-      <div className="entry__header">
-        <div className="entry__header-title">
-          <Link to={{ pathname: `/blog/post/${slug}` }}>{title}</Link>
-        </div>
-        <div className="entry__header-caption">{moment(publishedDate).format('MMM Do YYYY')}</div>
-      </div>
-      {image !== undefined ? <img className="entry__image" src={image.url} /> : null}
-      <div className="entry__body" dangerouslySetInnerHTML={{ __html: content.extended }} />
-      <div className="entry__footer">
-        <div className="entry__footer-author">By: <Link key={author.id} to={{ pathname: `/blog/authors/${author.name.first}` }}>{author.name.first}</Link></div>
-        <div className="entry__footer-categories">Posted in | {categories.map(item =>
-          <Link key={item.key} to={{ pathname: `/blog/categories/${item.key}` }}>{item.name}</Link>
-        )}</div>
-      </div>
+      {header}
+      {photo}
+      {body}
+      {footer}
     </div>
   );
 }
