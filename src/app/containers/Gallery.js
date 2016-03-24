b56c0a0aa2f985d001353264c46388acd5187533
Evan Turner <evturn@gmail.com>
2016-03-24 00:37:19 -0400

    Add styles to Gallery component

--- src/app/containers/Gallery.js
+++ src/app/containers/Gallery.js
@@ -1,12 +1,36 @@
 import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import classNames from 'classnames/bind';
+import css from 'less/components/gallery.less';
 
+const cx = classNames.bind(css);
 
 class Gallery extends Component {
   render() {
+    const { gallery } = this.props;
+
     return (
-      <div>Sup yalls</div>
+      <div className={cx('root')}>
+        <ul className={cx('galleries')}>{gallery.map(album =>
+          <li key={album.key} className={cx('album')}>
+            <div className={cx('box')}>
+              <div className={cx('hero')} style={{ backgroundImage: `url(${album.heroImage.secure_url})` }} />
+              <div className={cx('name')}>{album.name}</div>
+            </div>
+          </li>
+        )}</ul>
+      </div>
     );
   }
 }
 
-export default Gallery;
+Gallery.propTypes ={
+  gallery: PropTypes.array,
+  dispatch: PropTypes.func
+}
+
+export default connect(
+  state => ({
+    gallery: state.gallery
+  })
+)(Gallery);
