d43ac2632e345b8043168df3337d9e4a8fce61f9
Evan Turner <evturn@gmail.com>
2016-04-20 09:21:15 -0400

    Add icon font files

---
+++ src/server/updates/0.0.8-admins.js
@@ -0,0 +1,12 @@
+exports.create = {
+  User: [
+    {
+      name: 'Joe Face',
+      email: 'joe@face.com',
+      password: 'face',
+      username: 'joeface',
+      isAdmin: true,
+      custom: true
+    }
+  ]
+}
