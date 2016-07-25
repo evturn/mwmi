5635a39f2eeefbebfd49a41651abead04f21351f
Evan Turner <evturn@gmail.com>
2016-07-24 21:24:49 -0400

    Update autokey for Episode model

--- src/server/models/Episode.js
+++ src/server/models/Episode.js
@@ -7,7 +7,7 @@ const Episode = new List('Episode', {
     name: 'title'
   },
   autokey: {
-    path: 'slug',
+    path: 'key',
     from: 'title',
     unique: true,
   },
@@ -16,25 +16,29 @@ const Episode = new List('Episode', {
 Episode.add({
   title: {
     type: String,
+    label: 'Title',
     note: 'Tom Jones Swings By and Drinks a Jar of Ketchup',
-    initial: true,
-    required: true,
+    index: true,
   },
   image: {
     type: Field.Types.CloudinaryImage,
+    label: 'Image',
   },
   url: {
     type: Field.Types.Url,
     note: `Include 'http://' for external links. Leaving it off is interpretted as a link within your site.`,
+    label: 'URL',
   },
   state: {
     type: Field.Types.Select,
+    label: 'State',
     options: 'draft, published, archived',
     default: 'draft',
     index: true
   },
   publishedAt: {
     type: Field.Types.Date,
+    label: 'Publish Date',
     index: true,
     dependsOn: {
       state: 'published'
@@ -46,6 +50,7 @@ Episode.add({
   },
 })
 
-Episode.defaultSort = '-createdAt'
+
+Episode.defaultSort = '-publishedAt'
 Episode.defaultColumns = 'title|30%, image, url, state|10%'
 Episode.register()
