0978d38242328bbd19456e03d4f9d21188b2e97e
Evan Turner <ev@evturn.com>
2018-04-16 22:23:22 -0400

    Remove all deps and add CMS library

--- app/components/Footer/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import css from './styles.css'
-
-export default _ => (
-  <footer className={css.footer}>
-    Copyright 2016 © MWMI | All Rights Reserved
-  </footer>
-)
--- app/utils/connect.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default Component => {
-  return function componentWrapper(props) {
-    return function renderWithProps() {
-      return <Component { ...props } />
-    }
-  }
-}
