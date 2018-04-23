e46eb4464db1f0ae8d40c8855461f28f3ac853c5
Evan Turner <ev@evturn.com>
2018-04-23 17:55:17 -0400

    Add client side entry file

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = props => {
+  return (
+    <h1>HI MAN!</h1>
+  );
+};
+
+render(
+  <App />,
+  document.getElementById('root')
+);
