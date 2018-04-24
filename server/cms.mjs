5df1157dfb060dd76611bf2aadb3c4fba1c5d9b4
Evan Turner <ev@evturn.com>
2018-04-24 00:47:08 -0400

    Move database and cms configs to server directory

--- server/cms.mjs
+++ server/cms.mjs
@@ -1,8 +1,17 @@
 import cms from 'keystone';
-import cmsConfig from '../admin/config.mjs';
-import { User } from '../admin/models.mjs';
+import { User } from './models.mjs';
+import { pathTo } from '../tools/utils.mjs';
 
-cms.init(cmsConfig);
+cms.init({
+  'admin path': 'admin',
+  'cookie secret': 'hereismycookiesecret',
+  'user model': 'User',
+  auth: true,
+  mongo: 'mongodb://localhost/mwmi',
+  name: 'MWMI',
+  port: process.env.PORT_MWMI || 4000,
+  static: pathTo('build'),
+});
 
 const getAppInstance = appHandler => {
   cms.set('routes', appHandler);
