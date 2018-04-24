31ee7c86f1c7da5e48c531035ec125f5457bf515
Evan Turner <ev@evturn.com>
2018-04-24 16:30:28 -0400

    Add db model for Episode

--- server/models.mjs
+++ server/models.mjs
@@ -1,13 +1,64 @@
 import keystone from 'keystone';
 
-const { Field, List } = keystone;
-const User = new List('User');
+const { Field: { Types }, List } = keystone;
 
+const Episode = new List('Episode', {
+  map: {
+    name: 'title'
+  },
+  autokey: {
+    path: 'key',
+    from: 'title',
+    unique: true,
+  },
+})
+
+Episode.add({
+  title: {
+    type: String,
+    label: 'Title',
+    note: 'Tom Jones Swings By and Drinks a Jar of Ketchup',
+  },
+  image: {
+    type: Types.CloudinaryImage,
+    label: 'Image',
+  },
+  url: {
+    type: Types.Url,
+    note: `Include 'http://' for external links. Leaving it off is interpretted as a link within your site.`,
+    label: 'URL',
+  },
+  state: {
+    type: Types.Select,
+    label: 'State',
+    options: 'draft, published, archived',
+    default: 'draft',
+    index: true
+  },
+  publishedAt: {
+    type: Types.Date,
+    label: 'Publish Date',
+    index: true,
+    dependsOn: {
+      state: 'published'
+    }
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+Episode.defaultSort = '-publishedAt'
+Episode.defaultColumns = 'title|30%, image, url, state|10%'
+Episode.register()
+
+const User = new List('User');
 User.add({
-  email:    { type: Field.Types.Email,    required: true, initial: true, index: true },
-  name:     { type: Field.Types.Name,     required: true, index: true },
-  password: { type: Field.Types.Password, required: true, initial: true, },
-  username: { type: String,               required: true, initial: true, unique: true },
+  email:    { type: Types.Email,    required: true, initial: true, index: true },
+  name:     { type: Types.Name,     required: true, index: true },
+  password: { type: Types.Password, required: true, initial: true, },
+  username: { type: String,         required: true, initial: true, unique: true },
 },
 'Permissions', { 
   isAdmin:  { type: Boolean, label: 'Can access Keystone', index: true }
@@ -18,5 +69,6 @@ User.defaultColumns = 'name, email, username, isAdmin';
 User.register();
 
 export {
+  Episode,
   User
 };
