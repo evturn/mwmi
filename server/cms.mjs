e0185d275a72dbd65ae3864ae5d0d21e75d02fcd
Evan Turner <ev@evturn.com>
2018-04-24 16:43:57 -0400

    Create api endpoint and db query

--- server/cms.mjs
+++ server/cms.mjs
@@ -1,5 +1,5 @@
 import cms from 'keystone';
-import { User } from './models.mjs';
+import { Episode, User } from './models.mjs';
 import { pathTo } from '../tools/utils.mjs';
 
 cms.init({
@@ -13,8 +13,22 @@ cms.init({
   static: pathTo('build'),
 });
 
+const api = (req, res, next) => {
+  cms.list('Episode')
+    .model.find()
+    .where('state', 'published')
+    .sort('-publishedAt')
+    .exec((err, episodes) => {
+      res.locals.episodes = episodes
+      res.json(res.locals)
+  });
+};
+
 const getAppInstance = appHandler => {
-  cms.set('routes', appHandler);
+  cms.set('routes', app => {
+    appHandler(app);
+    app.get('/api/episodes', api);
+  });
   cms.start();
 };
 
