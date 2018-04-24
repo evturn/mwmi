cf17a1be975cacf439eabf63482b84decd2fee64
Evan Turner <ev@evturn.com>
2018-04-24 16:44:39 -0400

    Fetch data in home screen

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Episodes from './Episodes';
 import Logo from './Logo';
 
-const Home = props => {
-  return (
-    <div>
-      <Logo />
-      <Episodes />
-    </div>
-  );
-};
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      episodes: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch('/api/episodes')
+      .then(x => x.json())
+      .then(episodes => 
+        this.setState({ episodes }));
+  }
+
+  render() {
+    return (
+      <div>
+        <Logo />
+        <Episodes episodes={this.state.episodes} />
+      </div>
+    );
+  }
+}
 
 export default Home;
