1e742682e5f9c0e74e0902fed6b2284437c24abd
Evan Turner <ev@evturn.com>
2018-04-25 04:34:13 -0400

    Add episodes section to site

--- src/screens/Home/Episodes/index.js
+++ src/screens/Home/Episodes/index.js
@@ -4,9 +4,25 @@ import styles from './style.css';
 const Episodes = ({ episodes }) => {
   return (
     <div className={styles.root}>
-      <h2 className={styles.title}>Episodes</h2>
-      {episodes.map(x =>
-        <div key={x._id}>{x.title}</div>)}
+      <h2 className={styles.header}>Episodes</h2>
+      <div className={styles.items}>
+        {episodes.map(x =>
+          <div 
+            className={styles.item}
+            key={x._id}>
+            <a 
+              className={styles.link}
+              href={x.url}
+              target="_blank">
+              <img
+                className={styles.img}
+                src={x.image.secure_url} />
+              <div className={styles.title}>
+                {x.title}
+              </div>
+            </a>
+          </div>)}
+      </div>
     </div>
   );
 };
